93882d71905937fd4d5eb04e8834dd197cdccf93
cdickson21 <info@calebdickson.com>
2021-12-26 00:09:14 -0500

    created etch-a-sketch game, script and styling

---
+++ script.js
@@ -0,0 +1,32 @@
+let gridSize = 64
+
+const cells = document.querySelectorAll('cell');
+const colorPicker = document.getElementById('colorPicker');
+const gridSizePicker = document.querySelector('option');
+const resetButton = document.getElementById('reset');
+
+// let gridSize = function() {
+//    gridSizePicker.addEventListener('click',
+//    e => gridSize = gridSizePicker.value);
+//    return gridSize;
+// }
+
+window.onload = function() {
+   for (i = 1; i <= (gridSize*gridSize); i++) {
+      let d = document.createElement("div");
+      const wrapper = document.querySelector('.wrapper');
+      wrapper.style.cssText = `grid-template-columns: repeat(${gridSize}, 1fr);`
+      wrapper.appendChild(d);
+      d.className = 'cell';
+      d.id = `${i}`;
+      d.addEventListener('pointerover',
+      e => e.target.style.background = colorPicker.value);
+   }
+}
+
+function clearCanvas() {
+   resetButton.addEventListener('click', () =>
+   location.reload());
+}
+
+clearCanvas();
